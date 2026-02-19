da6ac866e10ed959626000d5d9ef7a2a8e8786a2
strmbaerchie <mm310794@gmail.com>
2026-02-19 10:53:17 +0100

    Add processRawData function in dataProcessors.ts

---
+++ utils/dataProcessors.ts
@@ -0,0 +1,8 @@
+import type { ProductionRecord, RoundingMode } from '../types';
+
+export function processRawData(
+  record: ProductionRecord,
+  roundingMode: RoundingMode
+): ProductionRecord {
+  return record;
+}
