80a697019246cafb7c5651c137cc0207cc7b757f
strmbaerchie <mm310794@gmail.com>
2026-02-19 10:43:46 +0100

    Add processRawData function to process production records
    Initial implementation of processRawData function.

---
+++ utils/dataProcessors.ts
@@ -0,0 +1,9 @@
+import type { ProductionRecord, RoundingMode } from '../types';
+
+export function processRawData(
+  records: ProductionRecord[],
+  roundingMode: RoundingMode
+): ProductionRecord[] {
+  // TODO: hier kommt später deine echte Logik rein
+  return records ?? [];
+}
